d9e45dbfbe39d8e9e9cb1f50ef5e60255634aa55
Davi <davi.dev2@hotmail.com>
2024-05-22 10:18:26 -0300

    segundo commit

--- aula11_heranca/src/Horista.java
+++ aula11_heranca/src/Horista.java
@@ -1,3 +1,20 @@
 public class Horista extends Empregado{
+    private double horas;
+
+    public double getHoras() {
+        return horas;
+    }
+
+    public void setHoras(double horas) {
+        this.horas = horas;
+    }
     
+    public Horista(String nome, String endereco, double salario, double horas) {
+        super(nome, endereco, salario);
+        this.horas = horas;
+    }
+
+    public void calcularHorista(){
+        salario = horas * 50;
+    }
 }
